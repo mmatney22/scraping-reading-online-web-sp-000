1c76fa4a2d3ca4b9b036feb9fecbfd7078aec804
Morgan Matney <morgan.matney89@gmail.com>
2020-09-14 14:30:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+#id-46503719-a9c3-5015-beac-801c618e339c > div > h2
+#id-46503719-a9c3-5015-beac-801c618e339c > div > h2
+#<h2 class="title-3Kf9MY" data-testid="title">Our partners</h2>
